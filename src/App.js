ac8ba2a83fdf03c46865846e202395e86b791ff4
Ronald Thomas <ronaldthomas094@gmail.com>
2022-01-28 12:26:26 +0530

    displaying multiple cards

--- src/App.js
+++ src/App.js
@@ -9,14 +9,18 @@ function App() {
   const [apiResponse, setApiResponse] = useState(null);
 
   useEffect(async () => {
-    try {
-      const response = await axios.get(
-        "http://www.omdbapi.com/?i=tt3896198&apikey=bf5d1178"
-      );
-      setApiResponse(response.data);
-    } catch (err) {
-      console.log(err);
+    let result = [];
+    for (let i = 1; i < 5; i++) {
+      try {
+        const response = await axios.get(
+          `http://www.omdbapi.com/?i=tt3896198&apikey=bf5d1178&s=Batman&page=${i}`
+        );
+        result.push(...response.data.Search);
+      } catch (err) {
+        console.log(err);
+      }
     }
+    setApiResponse(result);
   }, []);
 
   function setTitleHandler(value) {
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,16 +3,30 @@ import { Card } from "../UI/components";
 
 export function Home(props) {
   return (
-    <Card width="175px">
-      <img src={props?.apiResponse?.Poster} style={{ width: "inherit" }} />
-      <Text>{props?.apiResponse?.imdbRating}</Text>
-      <Text>{props?.apiResponse?.Title}</Text>
-    </Card>
+    <Container>
+      {props?.apiResponse?.map((movie) => (
+        <Card width="175px">
+          <img
+            src={movie.Poster}
+            style={{ width: "inherit", height: "260px" }}
+          />
+          <Text>{movie.Year}</Text>
+          <Text>{movie.Title}</Text>
+        </Card>
+      ))}
+    </Container>
   );
 }
 
 const Text = styled.h5`
   color: white;
   margin: 0;
-  padding: 10px;
+  padding: 5px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  margin: 10px 10%;
 `;
